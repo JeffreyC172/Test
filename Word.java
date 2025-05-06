382ba474cf228b5844da4b70a9a8ee75d55a0f93
JeffreyC172 <jeffreychan911@gmail.com>
2025-05-06 16:28:53 +0000

    v0.5

---
+++ Word.java
@@ -0,0 +1,54 @@
+public class Word
+{
+private String bannedWord="";
+private int timesBanned=1;
+private int daysSinceBan=0;
+private boolean banned;
+
+public Word(String bannedWord)
+ {
+this.bannedWord=bannedWord;
+banned=true;
+ }
+
+public int retTimesBanned()
+ {
+return timesBanned;
+ }
+
+public void ban(){
+timesBanned++;
+banned=true;
+ }
+
+public void dayPass()
+ {
+daysSinceBan++;
+ }
+
+ public boolean isBanned(){
+    return banned;
+ }
+
+public boolean doRemove()
+ {
+if (daysSinceBan>=7) {
+    return true;
+}
+return false;
+}
+
+public String getWord()
+ {
+return bannedWord;
+ }
+
+public String wordData(){
+return "Word: " + bannedWord + ". Is banned: " + banned + ". Banned" + timesBanned + "times.";
+}
+
+public void earlyRemove(){
+daysSinceBan=0;
+banned=false;
+}
+}
